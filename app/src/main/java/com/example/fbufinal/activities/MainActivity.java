15f2331a824441c2d934765badd160c9671f9320
Paulina LH <paulinalh2001@gmail.com>
2021-07-27 06:20:38 -0500

    Slide down panel for rating places

--- app/src/main/java/com/example/fbufinal/activities/MainActivity.java
+++ app/src/main/java/com/example/fbufinal/activities/MainActivity.java
@@ -11,6 +11,8 @@ import android.Manifest;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.example.fbufinal.R;
@@ -41,6 +43,9 @@ public class MainActivity extends AppCompatActivity implements PlacesAdapter.IPl
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //flContainer.onFindViewById(R.id.flContainer);
+
+        ListView listView=findViewById(R.id.listView);
+        listView.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, new String[]{"uno", "dos", "tres"}));
 
         BottomNavigationView bottomNavigation = (BottomNavigationView) findViewById(R.id.bottomNavigation);
         bottomNavigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
